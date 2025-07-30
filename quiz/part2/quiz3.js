c20bc5612834db55294a0547d41aede3c4373b2d
Raku <rakusama80@gmail.com>
2025-07-30 11:53:16 +0800

    menambahkan file-file quiz

---
+++ quiz/part2/quiz3.js
@@ -0,0 +1,12 @@
+let word3 = 'wow Javascript is so cool';
+let exampleFirstWord3 = word3.substring(0, 3);
+let secondWord3 = word3.substring(4, 15);
+let thirdWord3 = word3.substring(15, 17);
+let fourthWord3 = word3.substring(18, 20);
+let fifthWord3 = word3.substring(21, 25);
+console.log('First word: ' + exampleFirstWord3);
+console.log('Second word: ' + secondWord3);
+console.log('Third word: ' + thirdWord3);
+console.log('Fourth word: ' + fourthWord3);
+console.log('Fifth word: ' + fifthWord3);
+
---
+++ quiz/part2/quiz4.js
@@ -0,0 +1,21 @@
+let word4 = 'wow Javascript is so cool';
+
+let exampleFirstWord4 = word4.substring(0, 3);
+let firstWordLength = exampleFirstWord4.length;
+console.log('First word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
+
+let secondWord4 = word4.substring(4, 14);
+let secondWordLength = secondWord4.length;
+console.log('Second word: ' + secondWord4 + ', with length: ' + secondWordLength);
+
+let thirdWord4 = word4.substring(15, 17);
+let thirdWordLength = thirdWord4.length;
+console.log('Third word: ' + thirdWord4 + ', with length: ' + thirdWordLength);
+
+let fourthWord4 = word4.substring(18, 20);
+let fourthWordLength = fourthWord4.length;
+console.log('Fourth word: ' + fourthWord4 + ', with length: ' + fourthWordLength);
+
+let fifthWord4 = word4.substring(21, 25);
+let fifthWordLength = fifthWord4.length;
+console.log('Fifth word: ' + fifthWord4 + ', with length: ' +  fifthWordLength);
