23d28977fab2b52e2b4483c0bd99341932bb6f68
Raku <rakusama80@gmail.com>
2025-07-30 13:38:20 +0800

    menambahkan file file quiz

---
+++ quiz/part3/quiz1.js
@@ -0,0 +1,17 @@
+console.log('looping pertama');
+let i = 1;
+while (i <= 10) {
+    console.log(`forward looping: ` + i);
+    i++;
+}
+
+console.log('');
+
+
+console.log('looping kedua');
+let x = 10;
+while (1 <= x) {
+    console.log(`backward looping: ` + x);
+    --x;
+}
+
---
+++ quiz/part3/quiz2.js
@@ -0,0 +1,11 @@
+console.log('looping pertama')
+for (let i = 1; i <= 10; i++) {
+    console.log("forward looping: " + i);
+}
+
+console.log('');
+
+console.log('looping kedua')
+for (let x = 10 ; 1 <= x; x--) {
+    console.log(`backward looping: ` + x);
+}
