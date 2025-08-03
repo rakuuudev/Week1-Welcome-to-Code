3e155f959b585a8129b46af9b316fb35456fa7cc
Raku <rakusama80@gmail.com>
2025-08-03 15:41:35 +0800

    examquiz4 is done

---
+++ quiz/ujian/examquiz4.js
@@ -0,0 +1,20 @@
+function xo(str) {
+let x = 0;
+let o = 0;
+
+    for (let i = 0; i < str.length; i++) {
+    if (str[i] === 'x') {
+        x++;
+    } else if (str[i] === 'o') {
+        o++;
+    }
+
+    }
+    return x === o;
+}
+
+console.log(xo('xoxoxo')); //true
+console.log(xo('oxooxo')); //false
+console.log(xo('oxo')); //false
+console.log(xo('xxxooo')); //true
+console.log(xo('xoxooxxo')); //true
